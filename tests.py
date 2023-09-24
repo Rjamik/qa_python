c46e2e501d50b1b4fd7cc5e43d16f264be8bdd1e
Ramil S <lampus-r@yandex.ru>
2023-09-24 11:48:35 +0300

    new commit

--- tests.py
+++ tests.py
@@ -1,69 +1,65 @@
-import unittest
+import pytest
 from main import BooksCollector
 
-class TestBooksCollector(unittest.TestCase):
-
-    def setUp(self):
-        self.collector = BooksCollector()
-
-    def test_add_new_books(self):
-        self.collector.add_new_book("Мастер и Маргарита")
-        self.assertEqual(self.collector.books_genre, {"Мастер и Маргарита": ""})
-        self.collector.add_new_book("1984")
-        self.assertEqual(self.collector.books_genre, {"Мастер и Маргарита": "", "1984": ""})
-        self.collector.add_new_book("Мальчик, который поймал журавля")
-        self.assertEqual(self.collector.books_genre, {"Мастер и Маргарита": "", "1984": "", "Мальчик, который поймал журавля": ""})
-
-    def test_set_book_genre(self):
-        self.collector.add_new_book("Мастер и Маргарита")
-        self.collector.add_new_book("1984")
-        self.collector.set_book_genre("Мастер и Маргарита", "Фантастика")
-        self.assertEqual(self.collector.books_genre, {"Мастер и Маргарита": "Фантастика", "1984": ""})
-
-    def test_get_book_genre(self):
-        self.collector.add_new_book("1984")
-        self.collector.set_book_genre("1984", "Фантастика")
-        self.assertEqual(self.collector.get_book_genre("1984"), "Фантастика")
-
-    def test_get_books_with_specific_genre(self):
-        self.collector.add_new_book("Мастер и Маргарита")
-        self.collector.add_new_book("1984")
-        self.collector.set_book_genre("Мастер и Маргарита", "Фантастика")
-        self.collector.set_book_genre("1984", "Фантастика")
-        self.assertEqual(self.collector.get_books_with_specific_genre("Фантастика"), ["Мастер и Маргарита", "1984"])
-
-    def test_get_books_genre(self):
-        self.collector.add_new_book("Мастер и Маргарита")
-        self.collector.add_new_book("1984")
-        self.collector.set_book_genre("Мастер и Маргарита", "Фантастика")
-        self.assertEqual(self.collector.get_books_genre(), {"Мастер и Маргарита": "Фантастика", "1984": ""})
-
-    def test_get_books_for_children(self):
-        self.collector.add_new_book("Мастер и Маргарита")
-        self.collector.add_new_book("1984")
-        self.collector.set_book_genre("Мастер и Маргарита", "Фантастика")
-        self.collector.set_book_genre("1984", "Детективы")
-        self.assertEqual(self.collector.get_books_for_children(), ["Мастер и Маргарита"])
-
-    def test_add_book_in_favorites(self):
-        self.collector.add_new_book("1984")
-        self.collector.set_book_genre("1984", "Фантастика")
-        self.collector.add_book_in_favorites("1984")
-        self.assertEqual(self.collector.get_list_of_favorites_books(), ["1984"])
-
-    def test_delete_book_from_favorites(self):
-        self.collector.add_new_book("1984")
-        self.collector.set_book_genre("1984", "Фантастика")
-        self.collector.add_book_in_favorites("1984")
-        self.collector.delete_book_from_favorites("1984")
-        self.assertEqual(self.collector.get_list_of_favorites_books(), [])
-
-    def test_get_list_of_favorites_books(self):
-        self.collector.add_new_book("1984")
-        self.collector.set_book_genre("1984", "Фантастика")
-        self.collector.add_book_in_favorites("1984")
-        self.assertEqual(self.collector.get_list_of_favorites_books(), ["1984"])
+@pytest.fixture
+def collector():
+    return BooksCollector()
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_add_new_books(collector):
+    collector.add_new_book("Мастер и Маргарита")
+    assert collector.books_genre == {"Мастер и Маргарита": ""}
+    collector.add_new_book("1984")
+    assert collector.books_genre == {"Мастер и Маргарита": "", "1984": ""}
+    collector.add_new_book("Мальчик, который поймал журавля")
+    assert collector.books_genre == {"Мастер и Маргарита": "", "1984": "", "Мальчик, который поймал журавля": ""}
+
+def test_set_book_genre(collector):
+    collector.add_new_book("Мастер и Маргарита")
+    collector.add_new_book("1984")
+    collector.set_book_genre("Мастер и Маргарита", "Фантастика")
+    assert collector.books_genre == {"Мастер и Маргарита": "Фантастика", "1984": ""}
+
+def test_get_book_genre(collector):
+    collector.add_new_book("1984")
+    collector.set_book_genre("1984", "Фантастика")
+    assert collector.get_book_genre("1984") == "Фантастика"
+
+def test_get_books_with_specific_genre(collector):
+    collector.add_new_book("Мастер и Маргарита")
+    collector.add_new_book("1984")
+    collector.set_book_genre("Мастер и Маргарита", "Фантастика")
+    collector.set_book_genre("1984", "Фантастика")
+    assert collector.get_books_with_specific_genre("Фантастика") == ["Мастер и Маргарита", "1984"]
+
+def test_get_books_genre(collector):
+    collector.add_new_book("Мастер и Маргарита")
+    collector.add_new_book("1984")
+    collector.set_book_genre("Мастер и Маргарита", "Фантастика")
+    assert collector.get_books_genre() == {"Мастер и Маргарита": "Фантастика", "1984": ""}
+
+def test_get_books_for_children(collector):
+    collector.add_new_book("Мастер и Маргарита")
+    collector.add_new_book("1984")
+    collector.set_book_genre("Мастер и Маргарита", "Фантастика")
+    collector.set_book_genre("1984", "Детективы")
+    assert collector.get_books_for_children() == ["Мастер и Маргарита"]
+
+def test_add_book_in_favorites(collector):
+    collector.add_new_book("1984")
+    collector.set_book_genre("1984", "Фантастика")
+    collector.add_book_in_favorites("1984")
+    assert collector.get_list_of_favorites_books() == ["1984"]
+
+def test_delete_book_from_favorites(collector):
+    collector.add_new_book("1984")
+    collector.set_book_genre("1984", "Фантастика")
+    collector.add_book_in_favorites("1984")
+    collector.delete_book_from_favorites("1984")
+    assert collector.get_list_of_favorites_books() == []
+
+def test_get_list_of_favorites_books(collector):
+    collector.add_new_book("1984")
+    collector.set_book_genre("1984", "Фантастика")
+    collector.add_book_in_favorites("1984")
+    assert collector.get_list_of_favorites_books() == ["1984"]
